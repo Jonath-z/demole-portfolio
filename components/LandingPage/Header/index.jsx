8c9d109dcab04909ddb42a4048c48bbf0c4bc57c
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-15 20:46:21 +0200

    menu modal added on mobile

--- components/LandingPage/Header/index.jsx
+++ components/LandingPage/Header/index.jsx
@@ -1,9 +1,57 @@
-import React from "react";
-import { VMail, VMenu, VPhone } from "../../modules/vectos";
+import React, { useState } from "react";
+import { VMail, VMenu, VPhone, VPlus } from "../../modules/vectos";
 
 const Header = () => {
+  const [isMenu, setIsMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenu(!isMenu);
+  };
+
   return (
     <div>
+      <div
+        className={`fixed z-20 top-0 bottom-0 left-0 w-full backdrop-blur-md bg-black/30 lg:hidden ${
+          isMenu
+            ? "translate-x-0 transition-all"
+            : "translate-x-[2000000px] transition-all"
+        } `}
+      >
+        <ul className="lg:hidden flex flex-col gap-5 bg-white p-10 text-black rounded-b-lg relative">
+          <li
+            role="button"
+            onClick={toggleMenu}
+            className="absolute top-0 right-0 m-5 rounded-full border p-2"
+          >
+            <VPlus className="rotate-45" />
+          </li>
+          <a href="#about">
+            <li role="button" onClick={toggleMenu}>
+              A propos
+            </li>
+          </a>
+          <a href="#contact">
+            <li role="button" onClick={toggleMenu}>
+              Contacts
+            </li>
+          </a>
+          <a href="#service">
+            <li role="button" onClick={toggleMenu}>
+              Services
+            </li>
+          </a>
+          <a href="#feedback">
+            <li role="button" onClick={toggleMenu}>
+              Temoignage
+            </li>
+          </a>
+          <a href="#blog">
+            <li role="button" onClick={toggleMenu}>
+              Blog
+            </li>
+          </a>
+        </ul>
+      </div>
       <div className="flex justify-between items-center text-white bg-black/50 p-5  backdrop-blur-md">
         <div className="lg:block hidden">
           <ul className="hidden lg:flex gap-5">
@@ -24,7 +72,7 @@ const Header = () => {
             </a>
           </ul>
         </div>
-        <p className="lg:hidden">
+        <p role="button" onClick={toggleMenu} className="lg:hidden">
           <VMenu className="text-2xl" />
         </p>
         <ul className="flex items-center gap-4">
--- components/modules/vectos/index.js
+++ components/modules/vectos/index.js
@@ -1,8 +1,11 @@
 import { HiMail as VMail, HiMenuAlt4 as VMenu } from "react-icons/hi";
 import { FaPhoneAlt as VPhone } from "react-icons/fa";
-import { AiOutlineCopyright as VCopyWrite } from "react-icons/ai";
+import {
+  AiOutlineCopyright as VCopyWrite,
+  AiOutlinePlus as VPlus,
+} from "react-icons/ai";
 import {
   BsArrowLeft as VArrowLeft,
   BsArrowRight as VArrowRight,
 } from "react-icons/bs";
-export { VMail, VPhone, VMenu, VCopyWrite, VArrowLeft, VArrowRight };
+export { VMail, VPhone, VMenu, VCopyWrite, VArrowLeft, VArrowRight, VPlus };
